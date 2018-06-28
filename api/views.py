c7ea439dd1fa59c437d3fac29806e6504352c728
Pedro Ramos <ramuspedro@gmail.com>
2018-06-27 21:21:02 -0300

    api books

--- api/views.py
+++ api/views.py
@@ -6,6 +6,6 @@ from rest_framework import generics
 from books.models import Book
 from .serializers import BookSerializer
 
-class BookApiView(generics.ListAPIView):
+class BookAPIView(generics.ListAPIView):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
